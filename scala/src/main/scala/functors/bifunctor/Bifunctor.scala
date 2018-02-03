2765da53a1168279855f5bc3cc5f7fb1f071fdc1
robertbutacu <robert.butacu@yahoo.com>
2018-02-03 22:16:45 +0200

    bifunctor and laws functors examples

--- scala/src/main/scala/functors/bifunctor/Bifunctor.scala
+++
@@ -1,5 +0,0 @@
-package functors.bifunctor
-
-trait Bifunctor {
-
-}
--- scala/src/main/scala/functors/functor/Functor.scala
+++ scala/src/main/scala/functors/functor/Functor.scala
@@ -8,6 +8,8 @@ package functors.functor
   *   2. The composition of 2 functions and mapping the resulting function over a functor
   *     should be the same as mapping the first function over the functor and then the second one.
   *     fmap(f compose g, adt) === fmap(f, fmap(g, adt))
+  *
+  * Some functors include: List, Set, Map, Option, etc.
   */
 trait Functor[F[_]] {
   def fmap[A, B](fA: F[_])(f: A => B): F[B]

39b392ffbc9c1089f49a6927deae8c1e98c3e09e
robertbutacu <robert.butacu@yahoo.com>
2018-02-08 23:09:15 +0200

    added explanations changed signature of apply

--- scala/src/main/scala/functors/applicative/functor/ApplicativeFunctor.scala
+++ scala/src/main/scala/functors/applicative/functor/ApplicativeFunctor.scala
@@ -2,8 +2,13 @@ package functors.applicative.functor
 
 /**
   * Applicative is an intermediate between functor and monad.
+  *
+  * Its importance are dominated by:
+  *   - ability to apply functions of more parameters
+  *   - can apply functions wrapped into a functor context
+  *   - can combine multiple functors into one single product
   */
 
 trait ApplicativeFunctor[F[_]] {
-  def apply[A, B](f: F[A => B]): F[A] => F[B]
+  def apply[A, B](fa: F[A])(f: F[A => B]): F[A] => F[B]
 }

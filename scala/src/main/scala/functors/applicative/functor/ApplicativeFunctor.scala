3be64409ce9406fed9cc0af40ef34b33881d3ddf
robertbutacu <robert.butacu@yahoo.com>
2018-02-08 23:18:11 +0200

    added applicative functor explanation

--- scala/src/main/scala/functors/applicative/functor/ApplicativeFunctor.scala
+++ scala/src/main/scala/functors/applicative/functor/ApplicativeFunctor.scala
@@ -5,7 +5,17 @@ package functors.applicative.functor
   *
   * Its importance are dominated by:
   *   - ability to apply functions of more parameters
+  *     For example:
+  *       val f = (x: Int)(y: Int) => x + y
+  *       If we apply the above function to a functor Future(10).map(f),
+  *         the result will be a Future(x => 10 + x). And you can't map that any further.
+  *       A way to extract the result is needed, hence applicative functors, where you can do something like:
+  *       val f = (x: Int)(y: Int) => x + y
+  *       val r: Future[Int -> Int] = Future(10).map(f)
+  *       Future(20).apply(r) => should return 30
+  *
   *   - can apply functions wrapped into a functor context
+  *
   *   - can combine multiple functors into one single product
   */
 

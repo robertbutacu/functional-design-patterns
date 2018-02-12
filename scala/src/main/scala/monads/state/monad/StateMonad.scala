c3805576b1e70fd559f27c0fd150478c6f85905b
robertbutacu <robert.butacu@yahoo.com>
2018-02-12 19:51:42 +0200

    state monad definition

--- scala/src/main/scala/monads/state/monad/StateMonad.scala
+++ scala/src/main/scala/monads/state/monad/StateMonad.scala
@@ -1,5 +1,11 @@
 package monads.state.monad
 
-trait StateMonad {
 
+/**
+  * The equivalent of Memento design pattern in OOP on steroids,
+  *   state monads deal with maintaining previous states WHILE generating new ones.
+ */
+
+trait StateMonad[T, +A] {
+  def apply[S, A](f: S => (S, A)): StateMonad[S, A]
 }

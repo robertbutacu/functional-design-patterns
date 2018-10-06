8b8d1e8d3565fd1c034d2299020ffc2cbb4469bb
robertbutacu <robertbutacu@gmail.com>
2018-10-06 22:46:57 +0100

    Monad example

--- scala/src/main/scala/monads/monad/Monad.scala
+++ scala/src/main/scala/monads/monad/Monad.scala
@@ -1,6 +1,22 @@
 package monads.monad
 
+import scala.language.higherKinds
+
 trait Monad[M[_]] {
-  def map[S, T](f: T => S): M[S]
-  def flatMap[T,S](f: T => M[S]): M[S]
+  def pure[A](a: A): M[A]
+  def flatMap[A, B](input: M[A])(f: A => M[B]): M[B]
+}
+
+object Monad extends App {
+  implicit def optionMonad: Monad[Option] = new Monad[Option] {
+    override def pure[A](a: A): Option[A] = Some(a)
+
+    override def flatMap[A, B](input: Option[A])(f: A => Option[B]): Option[B] =
+      input.flatMap(f)
+  }
+
+  def applyFunction[M[_], A, B](input: M[A], f: A => M[B])(implicit m: Monad[M]): M[B] =
+    m.flatMap(input)(f)
+
+  println(applyFunction(Some(50), (x: Int) => if(x > 10) Some(10) else None))
 }

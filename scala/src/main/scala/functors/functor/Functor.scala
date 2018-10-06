6ce6c60f6d79059ce67c4be0aa7be97dd741662e
robertbutacu <robertbutacu@gmail.com>
2018-10-06 22:17:28 +0100

    Functor examples

--- scala/src/main/scala/functors/functor/Functor.scala
+++ scala/src/main/scala/functors/functor/Functor.scala
@@ -1,5 +1,7 @@
 package functors.functor
 
+import scala.language.higherKinds
+
 /**
   * ADT which aspires to be a functor must preserve 2 laws:
   *   1. fmap over the identity functor of the ADT should coincide with the original ADT
@@ -12,5 +14,23 @@ package functors.functor
   * Some functors include: List, Set, Map, Option, etc.
   */
 trait Functor[F[_]] {
-  def fmap[A, B](fA: F[_])(f: A => B): F[B]
+  def fmap[A, B](fA: F[A])(f: A => B): F[B]
+}
+
+object Functor extends App {
+  implicit def listFunctor: Functor[List] = new Functor[List] {
+    override def fmap[A, B](fA: List[A])(f: A => B): List[B] =
+      fA.map(f)
+  }
+
+  implicit def optionFunctor: Functor[Option] = new Functor[Option] {
+    override def fmap[A, B](fA: Option[A])(f: A => B): Option[B] = fA.map(f)
+  }
+
+  def transformList[F[_], A, B](list: F[A], f: A => B)(implicit functor: Functor[F]): F[B] = {
+    functor.fmap(list)(f)
+  }
+
+  println(transformList(List(1,2,3,4), (x: Int) => x + 1))
+  println(transformList(Option(4), (x: Int) => x + 1))
 }
